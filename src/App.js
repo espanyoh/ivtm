c1ca1553049a70e88d2ab2dc6bdc8ea026d375ed
Espanyoh <hotyoh@hotmail.com>
2016-12-05 00:15:48 +0700

    Change name to be mine

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	
+	constructor() {
+        super();
+        this.owner = {
+          title: 'Mr.',
+          name:'Espanyoh'
+        };
+    }
+	
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React : { this.owner.title } { this.owner.name }</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
