84d05de41863ed60513d636b564c358664434229
Espanyoh <hotyoh@hotmail.com>
2016-12-05 00:36:23 +0700

    Add react-bootstrap-table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'react-bootstrap-table/css/react-bootstrap-table.css';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +12,11 @@ class App extends Component {
           title: 'Mr.',
           name:'Espanyoh'
         };
+        this.products = [{
+            id : 1,
+            name : "yoh",
+            last : "ESP"
+        }]
     }
 	
   render() {
@@ -22,6 +29,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <BootstrapTable data={this.products} striped hover condensed>
+      <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
+      <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+      <TableHeaderColumn dataField='last'>Product Price</TableHeaderColumn>
+  </BootstrapTable>
+
       </div>
     );
   }
