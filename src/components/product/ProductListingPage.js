25f21ca4b456f0fec031b2d59d02782f47645ff4
Espanyoh <hotyoh@hotmail.com>
2016-12-11 19:46:29 +0700

    Update Product list page with dynamic list

--- src/components/product/ProductListingPage.js
+++ src/components/product/ProductListingPage.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import { Table,Grid,Row,Col } from 'react-bootstrap';
 
-export default class ProductListing extends Component {
+export default class ProductListingPage extends Component {
 
   render() {
+    const categoryList = [
+        {code:"LG", name:"Legging",color:"Orange"},
+        {code:"DS", name:"Dress",color:"Green"},
+        {code:"SW", name:"Sweater",color:"Pink"},
+        {code:"CO", name:"Coat",color:"Orange"},
+        {code:"OT", name:"Other",color:"Green"},
+      ]
     return (
-      <div>
-        ProductListing .... underconstruction!
-      </div>
+    <Grid>
+      <Row>
+        <Col xs={3} md={2} />
+        <Col xs={12} md={8}>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Category Code</th>
+                <th>Category Name</th>
+                <th>Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {categoryList.map(function(row, i) {
+                return (
+                  <tr key={i}>
+                    <td>{i+1}</td>
+                    <td>{row.code}</td>
+                    <td>{row.name}</td>
+                    <td>{row.color}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Col>
+        <Col xs={3} md={2} />
+      </Row>
+    </Grid>
     );
   }
 }
